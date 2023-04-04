3e8c6c990fd82cebb1beb083d13902e04dcecc36
Esteban <esteban29712@gmail.com>
2023-04-04 00:27:08 -0400

    final/testing

---
+++ script.js
@@ -0,0 +1,9 @@
+import EmblaCarousel from 'embla-carousel'
+
+const emblaNode = document.querySelector('.embla')
+const options = { loop: false }
+
+const emblaApi = EmblaCarousel(emblaNode, options)
+
+console.log(emblaApi.slideNodes()) // Access API
+
